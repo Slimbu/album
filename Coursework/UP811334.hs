5ca3ffe75651c995bf6f6b63796ccb3dd31001c8
Slimbu <up811334@myport.ac.uk>
2019-03-20 13:45:15 +0000

    1

--- Coursework/UP811334.hs
+++ Coursework/UP811334.hs
@@ -5,6 +5,7 @@
 --
 import Data.Char
 import Data.List
+import Text.Printf
 
 --
 -- Types
@@ -75,6 +76,68 @@ testData = [("Greatest Hits","Queen", 1981, 6300000),
 --  Your functional code goes here
 --
 --
+displayAlbums :: Database -> String
+displayAlbums database = concat(map displayAlbum database)
+
+sameAlbum :: String -> Database -> Bool
+sameAlbum title (t, a, y, s)
+  | title = t == True
+  | otherwise = False
+
+--search for album by title
+searchAlbum :: String -> Database -> Database
+searchAlbum title database = filter (sameAlbum title) database
+
+--display album as string
+displayAlbumString :: Database -> String
+displayAlbumString database = concat(map displayAlbumString database)
+
+displayAlbumString :: Album -> String
+displayAlbumString database (title, director, year, sales) = printf "\n%-30s %-30s %-14d %-14d" title director  year sales
+
+displayAlbum :: Album -> String
+displayAlbum (title, artist, year, sales) = printf "\n%-30s %-30s %-14d %-14d" title artist year sales
+
+--return sale of the album
+getSaleOfAlbum :: Film -> String
+getSaleOfAlbum (title, album, year, sales) :: Int
+
+--return sale of the album
+getSale :: Database -> Int
+getSale database = sum(map getAlbumSale database) / fromIntegral(length database)
+
+hasSale :: Int -> Album -> Bool
+hasSale sales (t, a, y, s)
+ | fromIntegral(length l) / (fromIntegral(length l) + fromIntegral(length dl)) > 10 = True
+ | otherwise = False
+
+--check that the person artist
+hasArtist :: String -> Album -> Bool
+hasArtist artist (t, a, y, s)
+ | artist == a = True
+ | otherwise = False
+
+--return true if year is between two years
+betweenYears :: Int -> Int -> Album -> Bool
+betweenYears minY maxY (t, a, y, s)
+  | y <= maxY && y >= minY = True
+  | otherwise = False
+
+
+albumSort (t1, a1, y1, s1) (t1, a1, y1, s1)
+  | fromIntegral(length s1) < (fromIntegral(length s2) / (fromIntegral(length s2) + fromIntegral(length s2))) = GT
+  | otherwise = LT
+
+
+addAlbum :: Album -> Database -> Database
+addAlbum album database = album : database
+
+--display all album
+displayAlbumByString :: Album -> Database -> Database
+displayAlbumByString artist database = displayAlbumByString (filter (hasArtist artist) database)
+
+betweenYearSort :: Int -> Int -> Database -> Database
+betweenYearSort minYear maxYear database = sortBy albumSort(filter (betw))
 
 
 
@@ -83,6 +146,7 @@ testData = [("Greatest Hits","Queen", 1981, 6300000),
 
 --demo :: Int -> IO ()
 --demo 1  = putStrLn (albumsToString testData)
+--demo 1 = putStrLn (displayAlbumString testData)
 --demo 2  = putStrLn (albumsToString (top10 testData))
 --demo 3  = putStrLn ( all albums released between 2000 and 2008 inclusive )
 --demo 4  = putStrLn ( all albums with titles beginning with "Th" )
@@ -97,3 +161,64 @@ testData = [("Greatest Hits","Queen", 1981, 6300000),
 -- Your user interface (and loading/saving) code goes here
 --
 --
+main :: IO ()
+main = do db <- readFile "albums.txt"
+          putStrLn "Enter your name: "
+	  let database=read db:: [Album]
+          userName <- getLine
+	  database <- userInterface (userName, database)
+          writeFile "albums.txt" (show database)
+          putStrLn "\n\nYour changes to the database has been successfull. :)"
+userInterface :: (String, Database) -> IO Database
+userInterface (userName, database) = do let info = (userName, database)
+                                        let message1 = "Press Enter to go back to the main menu: "
+                                        putStrLn "  Album Database  "
+                                        putStrLn "1. - convert the list into a single string which, if output using putStrLn, will display the data formatted neatly into four columns"
+                                        putStrLn "2. - give the top 10 albums in descending order of sales"
+                                        putStrLn "3. - give all albums that were released between two given years (inclusive)"
+                                        putStrLn "4. - give all albums whose titles begin with a given prefix"
+                                        putStrLn "5. - give the total sales figure for a given artist (i.e. the sum of the sales figures of the artist’s albums)"
+                                        putStrLn "6. - give a list of pairs of artists’ names with the number of albums they have in the top 50 (each artist should appear exactly once in the result)"
+                                        putStrLn "7. - remove the 50th (lowest-selling) album and add a given (new) album into the list (which may be placed higher than 50th place depending on its sales figure)"
+                                        putStrLn "8. - increase the sales figure for one of the albums given its title & artist and the additionalsales, possibly changing the album’s position in the list (if no album with the given details exists, the function should do nothing)"
+                                        putStrLn "0. - Exit and update database"
+                                        putStrLn "-----------------------------------------------------------------------------------------------------------------------------------------\n"
+                                        putStr "Enter a number to perform an action or 0 to exit and save the database: "
+                                        input <- getLine
+                                        if input /= "0"
+                                           then case input of
+                                                     "1" -> do info <- selection 1 info
+                                                               putStr message1
+                                                               entry <- getLine
+                                                               userInterface info
+                                                     "2" -> do info <- selection 2 info
+                                                               putStr message1
+                                                               entry <- getLine
+                                                               userInterface info
+                                                     "3" -> do info <- selection 3 info
+                                                               putStr message1
+                                                               entry <- getLine
+                                                               userInterface info
+                                                     "4" -> do info <- selection 4 info
+                                                               putStr message1
+                                                               entry <- getLine
+                                                               userInterface info
+                                                     "5" -> do info <- selection 5 info
+                                                               putStr message1
+                                                               entry <- getLine
+                                                               userInterface info
+                                                     "6" -> do info <- selection 6 info
+                                                               putStr message1
+                                                               entry <- getLine
+                                                               userInterface info
+                                                     "7" -> do info <- selection 7 info
+                                                               putStr message1
+                                                               entry <- getLine
+                                                               userInterface info
+                                                     "8" -> do info <- selection 8 info
+                                                               putStr message1
+                                                               entry <- getLine
+                                                               userInterface info
+                                                     _ -> do putStrLn "You have entered an invalid number."
+                                                             userInterface info
+                                        else return (snd info)

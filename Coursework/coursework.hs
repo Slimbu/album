f54cc9a547f1c558292be1a4ad52e36796049f89
Slimbu <up811334@myport.ac.uk>
2019-03-25 15:53:45 +0000

    2

--- Coursework/coursework.hs
+++ Coursework/coursework.hs
@@ -1,4 +1,26 @@
-("Greatest Hits","Queen", 1981, 6300000),
+--
+-- MATHFUN
+-- Template for the Haskell assignment program (replace this comment)
+-- Add your student number
+--
+import Data.Char
+import Data.List
+import Text.Printf
+
+--
+-- Types
+--
+type Title = String
+type Artist = String
+type Year = Int
+type Sales = Int
+
+-- Define Album type here
+type Album = (Title, Artist, Year, Sales)
+type Database = [Album]
+
+testData :: [Album]
+testData = [("Greatest Hits","Queen", 1981, 6300000),
             ("Gold: Greatest Hits","ABBA", 1992, 5400000),
             ("Sgt. Pepper's Lonely Hearts Club Band","The Beatles", 1967, 5340000),
             ("21", "Adele", 2011, 5110000),
@@ -44,7 +66,35 @@
             ("Parachutes", "Coldplay", 2000, 2710000),
             ("The Man Who", "Travis", 1999, 2687500),
             ("Greatest Hits", "ABBA", 1975, 2606000),
-            ("I've Been Expecting You", "Robbie Williams"     1998, 2586500),
+            ("I've Been Expecting You", "Robbie Williams", 1998, 2586500),
             ("Come Away with Me", "Norah Jones", 2002, 2556650),
             ("Graceland", "Paul Simon", 1986, 2500000),
-            ("Ladies & Gentlemen: The Best of", "George Michael", 1998, 2500000)
+            ("Ladies & Gentlemen: The Best of", "George Michael", 1998, 2500000)]
+
+--
+--
+--  Your functional code goes here
+--
+--
+
+
+-- Demo function to test basic functionality (without persistence - i.e.
+-- testData doesn't change and nothing is saved/loaded to/from albums file).
+
+--demo :: Int -> IO ()
+--demo 1 = putStrLn (albumsToString testData)
+--demo 1 = putStrLn (displayAlbumString testData)
+--demo 2  = putStrLn (albumsToString (top10 testData))
+--demo 3  = putStrLn ( all albums released between 2000 and 2008 inclusive )
+--demo 4  = putStrLn ( all albums with titles beginning with "Th" )
+--demo 5  = putStrLn ( total sales figure for "Queen" )
+--demo 6  = putStrLn ( all artists with the number of times they appear in top 50 )
+--demo 7  = putStrLn ( albums after removing 50th album and adding "Progress"
+--                     by "Take That" from 2010 with 2700000 sales )
+--demo 8  = putStrLn ( albums after increasing sales of "21" by "Adele" by 400000 )
+
+--
+--
+-- Your user interface (and loading/saving) code goes here
+--
+--
